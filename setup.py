eb29195e5db4d32e668d73f36a07c0ebbc169a81
aniko33 <gtabro666@gmail.com>
2024-01-30 00:08:32 +0100

    New version of package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='stone',
-    packages=find_packages(include=["stone"]),
-    version='0.1.0',
+    name='stone_color',
+    packages=find_packages(),
+    version='0.1.3',
     description='Text formatting, coloring and tables library',
     keywords=["color", "cli", "table", "terminal-colors", "ansi", "rgb"],
     author='aniko33',
