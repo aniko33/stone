b413f62f219dcbb89346d5015830757ad206f000
aniko33 <76649588+aniko33@users.noreply.github.com>
2024-02-01 17:24:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='stone_color',
     packages=find_packages(),
     version='0.1.7',
-    long_description='Stone_color is a style library for Python that can perform text formatting, coloring, tables, and loading bars.'
+    long_description='Stone_color is a style library for Python that can perform text formatting, coloring, tables, and loading bars.',
     description='Text formatting, coloring and tables library',
     keywords=["color", "cli", "table", "terminal-colors", "ansi", "rgb"],
     author='aniko33',
